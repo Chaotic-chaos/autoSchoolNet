cf89846c97551d444490c781dfe54845fce88aa3
Chaos <32286124+Chaotic-chaos@users.noreply.github.com>
2020-11-15 21:26:10 +0800

    Delete username

--- logout.sh
+++ logout.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-a=`curl http://222.197.192.59:9090/zportal/logout --data "userName=20202204294&userIp=10.33.130.72&deviceIp=172.16.32.254&service.id=&autoLoginFlag=false&userMac=f460e2fe1020&operationType=&isMacFastAuth=false"`
+a=`curl http://222.197.192.59:9090/zportal/logout --data "userName=!!!&userIp=10.33.130.72&deviceIp=172.16.32.254&service.id=&autoLoginFlag=false&userMac=f460e2fe1020&operationType=&isMacFastAuth=false"`
 b=`echo $a | grep -P '本次上网时间.*?秒' -o`
 c=`echo $b | sed 's/ //g'`
 curl -G -d "text="$c https://sc.ftqq.com/SCU44734Tc79e0c4d145033cf8d45de746707e1955c6784f0a55fa.send
